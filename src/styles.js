b10a70792035eefd36165048a96af04ccd38f907
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2025-12-17 05:04:36 +1100

    remove unused stylesheets

--- src/styles.js
+++ src/styles.js
@@ -7,6 +7,4 @@ import "./styles/tokens.css";
 import "./styles/base.css";
 import "./styles/layout.css";
 import "./styles/components.css";
-import "./styles/carousel.css";
-import "./styles/dropdown.css";
 import "./styles/utilities.css";
